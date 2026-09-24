bbb88b02fc25b7cd38272f58788009f97b5d838f
Klimentii Yudin <klimatroniks.pr@gmail.com>
2026-09-24 19:02:09 +0300

    found results/

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,12 +1,7 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
+import csv
+from datetime import datetime
+from collections import Counter
+from pathlib import Path
 
 class PepParsePipeline:
     def process_item(self, item):
