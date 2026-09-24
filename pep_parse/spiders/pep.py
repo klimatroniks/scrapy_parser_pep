d390a1c0613620301914a083a04bdb545360a537
Юдин Климентий <yudin_ka@finntrail.local>
2026-09-24 10:17:49 +0300

    create logic spider pep

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,27 @@
+from pep_parse.items import PepParseItem
 import scrapy
 
 
 class PepSpider(scrapy.Spider):
-    name = "pep"
-    allowed_domains = ["peps.python.org"]
-    start_urls = ["https://peps.python.org"]
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        links = response.css('')
+
+        for link in links:
+            yield response.follow(
+                link,
+                callback=self.parse_pep
+            )
+
+    def parse_pep(self, response):
+        title = response.css('').get()
+        status = response.css('').get()
+
+        yield PepParseItem(
+            number=number,
+            name=name,
+            status=status
+        )
