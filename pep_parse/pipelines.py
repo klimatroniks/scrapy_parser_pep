29d038bad72ef4f3f9063bc5c215de9ed0a702fe
Klimentii Yudin <klimatroniks.pr@gmail.com>
2026-09-25 17:18:29 +0300

    fix mistake

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -11,12 +11,36 @@ from pep_parse.settings import (
 
 class PepParsePipeline:
     def __init__(self):
-        self.statuses = Counter()
         RESULTS_DIR.mkdir(
             parents=True,
             exist_ok=True
         )
 
+    def open_spider(self, spider):
+        self.statuses = Counter()
+
     def process_item(self, item, spider):
         self.statuses[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        timestamp = datetime.now().strftime(DATETIME_FORMAT)
+        filename = (
+            RESULTS_DIR / STATUS_SUMMARY_FILENAME.format(timestamp)
+        )
+
+        with open(
+            filename,
+            mode='w',
+            encoding='utf-8',
+            newline=''
+        ) as file:
+            writer = csv.writer(file, dialect=csv.excel)
+
+            rows = [
+                ['Статус', 'Количество'],
+                *self.statuses.items(),
+                ['Total', sum(self.statuses.values())],
+            ]
+
+            writer.writerows(rows)
